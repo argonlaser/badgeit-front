fed9b8c3c1157da0b045fd6cec4f74b0c7721362
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-07-06 15:35:29 +0530

    Removes /slug endpoint and tests to start fresh

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 const Hapi = require('hapi');
 const Inert = require('inert');
-const Lout = require('lout');
 const Vision = require('vision');
 const Routes = require('./routes');
 
@@ -13,12 +12,7 @@ const port = 8080;
 const host = '0.0.0.0' || 'localhost';
 server.connection({ port: port, host: host });
 
-const loutRegister = {
-    register: Lout,
-    options: { endpoint: '/docs' }
-};
-
-server.register([Vision, Inert, loutRegister], function(err) {
+server.register([Vision, Inert], function(err) {
 
     if (err) {
         console.error('Failed loading plugins');
