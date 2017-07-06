7c6491a78606cd006b64f5546b088d190c6c084f
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-07-06 15:19:41 +0530

    Adds a generic handler for repoNames

--- routes.js
+++ routes.js
@@ -62,11 +62,26 @@ internals.serveHomePage = function(request, reply) {
     reply.file('views/home.html');
 }
 
+internals.serveResultPage = function(request, reply) {
+    var repoName = request.params.repoName;
+
+    // user repoName to do all computations
+    console.log(repoName);
+
+    reply.file('views/result.html')
+}
+
 module.exports = [{
-  method: 'GET',
-  path: '/',
-  handler: internals.serveHomePage
-  }, {
+    method: 'GET',
+    path: '/',
+    handler: internals.serveHomePage
+},
+{
+    method: 'GET',
+    path: '/{repoName*}',
+    handler: internals.serveResultPage
+},
+{
     method: 'GET',
     path: '/slugs',
     config: {
