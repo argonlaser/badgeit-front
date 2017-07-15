5fe5d69f299d24c42bd6605901f963c17f69e501
Venkata Krishna.S <vkvenkat94@gmail.com>
2017-07-15 10:59:44 +0530

    working socketio.

--- routes.js
+++ routes.js
@@ -1,12 +1,15 @@
 const superagent = require('superagent')
 
 let internals = {}
+let clients = []
 
 internals.serveHomePage = function (request, reply) {
+  console.log('serveHomePage | Serve Home')
   reply.file('views/home.html')
 }
 
 internals.serveFavicon = function (request, reply) {
+  console.log('serveFavicon | Serve Favicon')
   reply('success')
 }
 
@@ -14,28 +17,51 @@ internals.serveResultPage = function (request, reply) {
   var repoName = request.params.repoName
   var API_BASE_URL = 'http://34.211.102.93'
   var domain
-  console.log('ENV', process.env.NODE_ENV)
-  if (process.env.NODE_ENV) {
-    domain = 'http://0a6e08c6.ngrok.io'
+  console.log('serveResultPage',' | ', 'ENV : ', process.env.NODE_ENV)
+  if (process.env.NODE_ENV === 'production') {
+    domain = 'http://b45baf6c.ngrok.io'
   } else {
     domain = 'http://localhost:8080'
   }
-  var CALLBACK_URL = domain + '/callback'
+  var CALLBACK_URL = 'https://05822d6a.ngrok.io' + '/callback'
 
-console.log(CALLBACK_URL)
+  console.log('serveResultPage', '|', 'callback url:', CALLBACK_URL)
 
   superagent
     .get(API_BASE_URL + '/badges')
       .query({ download: 'git', remote: repoName, callback: CALLBACK_URL }) // query string
       .end(function (err, res) {
         // Do something
+        console.log('POST /badges | ', 'Error: ', err )
         reply.file('views/result.html')
       })
 }
 
 internals.handleCallback = function (request, reply) {
-  console.log('Trying to connect to socket', request.payload)
-  global.io.sockets.emit('news', { reqData: request.payload })
+  console.log('In handleCallback | ' + request.payload)
+  var badges = request.payload
+  global.io.sockets.on('connection', function(socket) {
+      // Socket connected
+      console.log('Socket connected| (New client id=' + socket.id + ').');
+
+      // Save the socket id to redis store
+      clients.push(socket)
+
+      // Send an event once socket is connected
+      console.log('Data emitted: | (New client id=' + socket.id + ').');
+      socket.emit('news', { reqData: request.payload })
+
+      // Remove the socket on disconnection
+      socket.on('disconnect', function() {
+            var socketIndex = clients.indexOf(socket);
+            if (socketIndex != -1) {
+                clients.splice(socketIndex, 1);
+                console.info('Client gone (id=' + socket.id + ').');
+            }
+      })
+
+  });
+//  global.io.sockets.emit('badges received', { reqData: request.payload })
 
   reply('success')
 }
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const Hapi = require('hapi')
 const Inert = require('inert')
 const Vision = require('vision')
 const Routes = require('./routes')
+var sio = require("socket.io");
+
 const Https = {
   register: require('hapi-require-https'),
   options: {}
@@ -15,7 +17,47 @@ const host = process.env.BADGEIT_FRONT_HOST
 console.log('START:', port, host)
 
 server.connection({ port: port, host: host })
-const io = require('socket.io')(server.listener)
+
+const io = sio(server.listener)
+global.io = io
+global.clients = []
+
+let clients = []
+//
+// global.io.sockets.on('connection', function(socket) {
+//     console.log('Socket connected| (New client id=' + socket.id + ').');
+//     // Save the socket id to redis store
+//     global.clients.push(socket)
+//
+//     // Send an event once socket is connected
+//   socket.on('news', global.newsHandler(socket, data))
+// })
+
+// io.on('connection', function (socket) {
+//   socket.emit('news', { hello: 'world' });
+//   socket.on('my other event', function (data) {
+//     console.log(data);
+//   });
+// });
+
+// In this example we have one master client socket
+// that receives messages from others.
+
+// io.sockets.on('connection', function(socket) {
+//   console.log('Socket connected| (New client id=' + socket.id + ').');
+//   // Save the socket id to redis store
+//   clients.push(socket)
+//
+//   // Remove the socket on disconnection
+//   socket.on('disconnect', function() {
+//         var socketIndex = clients.indexOf(socket);
+//         if (socketIndex != -1) {
+//             clients.splice(socketIndex, 1);
+//             console.info('Client gone (id=' + socket.id + ').');
+//         }
+//     });
+
+// });
 
 server.register([Vision, Inert, Https], function (err) {
   if (err) {
@@ -30,5 +72,4 @@ server.register([Vision, Inert, Https], function (err) {
   })
 })
 
-global.io = io
 module.exports = server
