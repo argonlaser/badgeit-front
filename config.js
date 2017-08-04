a462e0e288f07b632c1b5c9e20843bc5e898efa2
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-08-04 23:28:44 +0530

    adds env for port and host

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ module.exports =
     'FRONT_URL': process.env.FRONT_URL || 'https://badgeit-front.now.sh',
     'API_BASE_URL': process.env.API_BASE_URL || 'http://34.211.102.93',
     'SERVER': {
-      'port': 8080,
-      'host': 'localhost'
+      'port': process.env.PORT || 8080,
+      'host': process.env.HOST || 'localhost'
     }
   },
   'test': {
