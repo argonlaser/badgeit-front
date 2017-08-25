cb2e28632688a4a27afbdc3e3070bec29797c3d6
vinoth kumar <sarvam.vinoth@gmail.com>
2017-08-26 01:24:41 +0530

    :changed

--- config.js
+++ config.js
@@ -1,24 +1,24 @@
 module.exports =
 {
   'development': {
-    'FRONT_URL': 'https://644de343.ngrok.io',
-    'API_BASE_URL': 'http://34.211.102.93',
+    'FRONT_URL': 'https://75f59aaa.ngrok.io',
+    'API_BASE_URL': 'http://34.211.102.93:8080',
     'SERVER': {
       'port': 8080,
       'host': 'localhost'
     }
   },
   'production': {
-    'FRONT_URL': 'https://badgeit-front.now.sh',
-    'API_BASE_URL': 'http://34.211.102.93',
+    'FRONT_URL': process.env.FRONT_URL || 'https://badgeit-front.now.sh',
+    'API_BASE_URL': process.env.API_BASE_URL || 'http://34.211.102.93',
     'SERVER': {
-      'port': 8080,
-      'host': 'localhost'
+      'port': process.env.PORT || 8080,
+      'host': process.env.HOST || 'localhost'
     }
   },
   'test': {
     'FRONT_URL': 'https://localhost:8080',
-    'API_BASE_URL': 'http://34.211.102.93',
+    'API_BASE_URL': 'http://34.211.102.93:8080',
     'SERVER': {
       'port': 8080,
       'host': 'localhost'
